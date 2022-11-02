b6a4de0730ff3d4b7f28d8fdb684ba15a4103452
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-11-02 10:25:52 -0500

    commit tree 40de7fa27f603cad484165b13bf07d01a026fb45

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+var copyOfCats = new Array;
+var copyOfCats1 = new Array;
+var copyOfCats2 = new Array;
+var copyOfCats3 = new Array;
+
+//var cats1 = [...cats, "Broom"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats1 = ["Arnold", ...cats];
+    return copyOfCats1;
+}
+
+function removeLastCat() {
+    const copyOfCats2 = cats.slice(0, -1);
+    return copyOfCats2;
+}
+
+function removeFirstCat() {
+    const copyOfCats3 = cats.slice(1);
+    return copyOfCats3;
+}
